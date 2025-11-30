c1721534e3fa9fd08f8124d86081be117c95ff95
okihara <okihara@gmail.com>
2025-11-30 19:40:59 +0900

    Enhance AssistantText component styling by updating background and text color for improved readability and visual consistency

--- src/components/assistantText.tsx
+++ src/components/assistantText.tsx
@@ -2,12 +2,9 @@ export const AssistantText = ({ message }: { message: string }) => {
   return (
     <div className="absolute bottom-0 left-0 mb-104  w-full">
       <div className="mx-auto max-w-4xl w-full p-16">
-        <div className="bg-white rounded-8">
-          <div className="px-24 py-8 bg-secondary rounded-t-8 text-white font-bold tracking-wider">
-            CHARACTER
-          </div>
+        <div className="bg-white rounded-8" style={{ boxShadow: '0px 0px 24px 19px #FFFFFF', opacity: 0.9 }}>
           <div className="px-24 py-16">
-            <div className="line-clamp-10 text-secondary typography-16 font-bold whitespace-pre-wrap">
+            <div className="line-clamp-10 text-black typography-16 font-bold whitespace-pre-wrap">
               {message.replace(/\[([a-zA-Z]*?)\]/g, "")}
             </div>
           </div>
