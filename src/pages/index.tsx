b447235743562eebefcfae2a6716438f4d5a5cb0
okihara <okihara@gmail.com>
2025-11-30 17:47:11 +0900

    Remove powered by text and GitHub link from MessageInput and Home components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,6 @@ import { KoeiroParam, DEFAULT_PARAM } from "@/features/constants/koeiroParam";
 import { getChatResponseStream } from "@/features/chat/openAiChat";
 import { Introduction } from "@/components/introduction";
 import { Menu } from "@/components/menu";
-import { GitHubLink } from "@/components/githubLink";
 import { Meta } from "@/components/meta";
 
 export default function Home() {
@@ -213,7 +212,6 @@ export default function Home() {
         handleClickResetSystemPrompt={() => setSystemPrompt(SYSTEM_PROMPT)}
         onChangeKoeiromapKey={setKoeiromapKey}
       />
-      <GitHubLink />
     </div>
   );
 }
